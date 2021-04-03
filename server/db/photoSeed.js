23e976c0f1b79a94e2599bff5aab88c7ace3e40f
Jake Carr <jcarr0157@gmail.com>
2021-04-03 09:48:26 -0700

    photo seed funtction

--- server/db/photoSeed.js
+++ server/db/photoSeed.js
@@ -1,8 +1,7 @@
-// seed photos table
 const db = require('./index')
+
 const profilePhotos = ['https://ca.slack-edge.com/T2SV1LBC6-U01K1S48C2Z-g2ea6c546d81-512', 'https://secure.gravatar.com/avatar/e3b51ca72dee4ef87916ae2b9240df50.jpg?s=512&d=https%3A%2F%2Fdev.slack.com%2Fimg%2Favatars%2Fava_0010-512.v1443724322.png']
 const dogPhotos = ['https://images.dog.ceo//breeds//cattledog-australian//IMG_3668.jpg', 'https://images.dog.ceo//breeds//cotondetulear//IMAG1063.jpg', 'https://images.dog.ceo//breeds//kuvasz//n02104029_2504.jpg', 'https://images.dog.ceo//breeds//retriever-chesapeake//n02099849_953.jpg', 'https://images.dog.ceo//breeds//boxer//n02108089_2791.jpg', 'https://images.dog.ceo//breeds//finnish-lapphund//mochilamvan.jpg', 'https://images.dog.ceo//breeds//beagle//DSC05086.JPG', 'https://images.dog.ceo//breeds//dhole//n02115913_1010.jpg', 'https://images.dog.ceo//breeds//poodle-toy//n02113624_265.jpg', 'https://images.dog.ceo//breeds//pug//DSCF7495-2.jpg']
-
 
 const seed = () => {
   for (let i = 1; i <= 2000; i++) {
