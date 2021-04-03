91686d4eff89fa12479dc6bab717f45a6e2c0265
Michael Banuelos <mbanuelos1995@gmail.com>
2021-04-03 15:23:33 -0700

    barebones profile view

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './Navbar';
 import Map from './Map';
-
+import ProfileView from './Homepage/ProfileVIew';
+import axios from 'axios';
 const App = () => {
+
+  const [currentUser, setCurrentUser] = useState('')
+  const getUser = () => {
+    axios.get('/app/users/my-profile/sophiaacheong4@gmail.com')
+      .then((data) => {
+        setCurrentUser(data.data[0])
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <Map />
+      <ProfileView user={currentUser} />
+      {/* <Map /> */}
     </div>
   )
 }
