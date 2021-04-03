dff0e2fc1ff35e09faacca926f7987f7c3b0caf9
hyungjooyoon <hyungjooyoon@gmail.com>
2021-04-03 14:27:19 -0700

    Started on map component. Used google map react package.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './Navbar';
+import Map from './Map';
 
 const App = () => {
   return (
     <div>
       Hi
       <NavBar />
+      <Map />
     </div>
   )
 }
