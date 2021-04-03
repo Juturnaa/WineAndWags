d1d270cfb5ba4f432f54af135f66481bac539204
jpdavid <justinepdavid@gmail.com>
2021-04-03 10:01:35 -0700

    Created routes for convos and for messages

--- server/router.js
+++ server/router.js
@@ -1,7 +1,19 @@
-const router = require("express").Router();
-const controller = require("./controller");
+const router = require('express').Router();
+const controller = require('./controller');
+
 module.exports = router;
 
-router.route("/users/my-profile").get(controller.getMyProfile);
+router.route('/users/my-profile').get(controller.getMyProfile);
 
-router.route("/users/random-profile").get(controller.getRandomProfile);
+router.route('/users/random-profile').get(controller.getRandomProfile);
+
+// Messages //
+router
+  .route('/messages')
+  .get(controller.getAllConvos)
+  .post(controller.postNewConvo);
+
+router
+  .route('/messages/:convo_id')
+  .get(controller.getConvoId)
+  .post(controller.postMessage);
