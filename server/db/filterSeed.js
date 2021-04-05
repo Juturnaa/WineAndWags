f2b8b0f097d5b2f4afadc9975ec68d38022a8b90
MAI <nmai601@gmail.com>
2021-04-05 10:44:09 -0700

    Fixed owner genders from both to all

--- server/db/filterSeed.js
+++ server/db/filterSeed.js
@@ -9,7 +9,7 @@ const hypos = [true, false];
 const neutereds = [true, false];
 const health_issuess = [true, false];
 const max_dists =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-const genders = ["M", "F", "Both"];
+const genders = ["M", "F", "All"];
 const min_ages = [18,19,20,21,22,23,24,25];
 const max_ages = [25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
 
