ce2273784c11da57a98f1cb8c89eeeaa88b822d1
Jake Carr <jcarr0157@gmail.com>
2021-04-05 12:00:29 -0700

    first pass at styling/layout for filter modal

--- client/src/components/Homepage/Filters.jsx
+++ client/src/components/Homepage/Filters.jsx
@@ -61,85 +61,97 @@ export default function Filters({
     return `${sizeLabels[first].label} - ${sizeLabels[second].label}`
   }
 
+  const sliderStyle = {
+    width: '50%'
+  }
+
   return (
-    <div>
-      <h3>Owner</h3>
-      <p>Max Distance: {maxDistance}</p>
-      <Slider value={maxDistance} onChange={(e, val) => changeMaxDistance(val)} aria-labelledby="continuous-slider" min={0} max={50} />
-      <p>Age Range: {ownerAgeRange[0]}-{ownerAgeRange[1]}</p>
-      <Slider value={ownerAgeRange} onChange={(e, val) => changeOwnerAgeRange(val)} aria-labelledby="range-slider" min={18} max={100} />
-      <p>Genders</p>
-      <FormControl component="fieldset">
-        <RadioGroup row aria-label="gender" name="gender_owner" value={ownerGenders} onChange={(e, val) => changeOwnerGenders(val)}>
-          <FormControlLabel value="M" control={<Radio />} label="Male" />
-          <FormControlLabel value="F" control={<Radio />} label="Female" />
-          <FormControlLabel value="All" control={<Radio />} label="All" />
-        </RadioGroup>
-      </FormControl>
-      <h3>Dog</h3>
-      <p>Size Range: {displaySizeRangeAsString(sizeRange[0], sizeRange[1])}</p>
-      <Slider
-        value={sizeRange}
-        onChange={(e, val) => changeSizeRange(val)}
-        marks={sizeLabels}
-        step={1}
-        min={0}
-        max={4}
-      />
-      <p>Age Range: {dogAgeRange[0]}-{dogAgeRange[1]}</p>
-      <Slider value={dogAgeRange} onChange={(e, val) => changeDogAgeRange(val)} aria-labelledby="range-slider" min={0} max={20} />
-      <p>Genders</p>
-      <FormControl component="fieldset">
-        <RadioGroup row aria-label="gender" name="gender_dog" value={dogGenders} onChange={(e, val) => changeDogGenders(val)}>
-          <FormControlLabel value="M" control={<Radio />} label="Male" />
-          <FormControlLabel value="F" control={<Radio />} label="Female" />
-          <FormControlLabel value="Both" control={<Radio />} label="Both" />
-        </RadioGroup>
-      </FormControl>
-      <p>Information</p>
-      <FormControlLabel
-        control={<Checkbox checked={hypoallergenic} onChange={() => changeHypoallergenic(!hypoallergenic)} name="hypoallergenic" />}
-        label="Hypoallergenic"
-      />
-      <FormControlLabel
-        control={<Checkbox checked={neutered} onChange={() => changeNeutered(!neutered)} name="neutered" />}
-        label="Neutered/Spayed"
-      />
-      <FormControlLabel
-        control={<Checkbox checked={healthIssues} onChange={() => changeHealthIssues(!healthIssues)} name="healthIssues" />}
-        label="Health issues"
-      />
-      <p>Avoid breeds:</p>
-      <FormControl>
-        <Select
-          multiple
-          value={avoidBreeds}
-          onChange={(e) => changeAvoidedBreeds(e.target.value)}
-          input={<Input />}
-        >
-          {breeds.map((breed) => (
-            <MenuItem key={breed} value={breed}>
-              {breed}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <p>Preferred breeds:</p>
-      <FormControl>
-        <Select
-          multiple
-          value={preferredBreeds}
-          onChange={(e) => changePreferredBreeds(e.target.value)}
-          input={<Input />}
-        >
-          {breeds.map((breed) => (
-            <MenuItem key={breed} value={breed}>
-              {breed}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <button onClick={() => alert('PUT REQUEST')}>Apply</button>
+    <div className='filter-modal'>
+      <div className='dog-filters'>
+        <h3>Dog</h3>
+        <p>Age Range: {dogAgeRange[0]}-{dogAgeRange[1]}</p>
+        <Slider style={sliderStyle} value={dogAgeRange} onChange={(e, val) => changeDogAgeRange(val)} aria-labelledby="range-slider" min={0} max={20} />
+        <p>Size Range: {displaySizeRangeAsString(sizeRange[0], sizeRange[1])}</p>
+        <Slider style={sliderStyle}
+          value={sizeRange}
+          onChange={(e, val) => changeSizeRange(val)}
+          marks={sizeLabels}
+          step={1}
+          min={0}
+          max={4}
+        />
+        <p>Genders</p>
+        <FormControl component="fieldset">
+          <RadioGroup row aria-label="gender" name="gender_dog" value={dogGenders} onChange={(e, val) => changeDogGenders(val)}>
+            <FormControlLabel value="M" control={<Radio />} label="Male" />
+            <FormControlLabel value="F" control={<Radio />} label="Female" />
+            <FormControlLabel value="Both" control={<Radio />} label="Both" />
+          </RadioGroup>
+        </FormControl>
+        <p>Information</p>
+        <FormControlLabel
+          control={<Checkbox checked={hypoallergenic} onChange={() => changeHypoallergenic(!hypoallergenic)} name="hypoallergenic" />}
+          label="Hypoallergenic"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={neutered} onChange={() => changeNeutered(!neutered)} name="neutered" />}
+          label="Neutered/Spayed"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={healthIssues} onChange={() => changeHealthIssues(!healthIssues)} name="healthIssues" />}
+          label="Health issues"
+        />
+        <p>Breeds:</p>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <p style={{marginRight: '1rem'}}>Avoid</p>
+          <FormControl>
+            <Select
+              multiple
+              value={avoidBreeds}
+              onChange={(e) => changeAvoidedBreeds(e.target.value)}
+              input={<Input />}
+              style={{marginRight: '1rem'}}
+            >
+              {breeds.map((breed) => (
+                <MenuItem key={breed} value={breed}>
+                  {breed}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <p style={{marginRight: '1rem'}}>Prefer</p>
+          <FormControl>
+            <Select
+              multiple
+              value={preferredBreeds}
+              onChange={(e) => changePreferredBreeds(e.target.value)}
+              input={<Input />}
+            >
+              {breeds.map((breed) => (
+                <MenuItem key={breed} value={breed}>
+                  {breed}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+      <div className='owner-filters'>
+        <h3>Owner</h3>
+        <p>Age Range: {ownerAgeRange[0]}-{ownerAgeRange[1]}</p>
+        <Slider style={sliderStyle} value={ownerAgeRange} onChange={(e, val) => changeOwnerAgeRange(val)} aria-labelledby="range-slider" min={18} max={100} />
+        <p>Max Distance: {maxDistance}</p>
+        <Slider style={sliderStyle} value={maxDistance} onChange={(e, val) => changeMaxDistance(val)} aria-labelledby="continuous-slider" min={0} max={50} />
+        <p>Genders</p>
+        <FormControl component="fieldset">
+          <RadioGroup row aria-label="gender" name="gender_owner" value={ownerGenders} onChange={(e, val) => changeOwnerGenders(val)}>
+            <FormControlLabel value="M" control={<Radio />} label="Male" />
+            <FormControlLabel value="F" control={<Radio />} label="Female" />
+            <FormControlLabel value="All" control={<Radio />} label="All" />
+          </RadioGroup>
+        </FormControl>
+        <button className='apply-btn' onClick={() => alert('PUT REQUEST')}>Apply changes</button>
+      </div>
     </div>
   )
 }
