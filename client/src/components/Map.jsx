58be8d3d5225fab67b485d9c435907d13c8d9ad1
hyungjooyoon <hyungjooyoon@gmail.com>
2021-04-05 19:56:10 -0700

    check to see server endpoint issue is a local problem.

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,25 +1,65 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-
-key = AIzaSyCSlzy473wIHHYArtLvphiwprDnzA3myKE;
+import axios from 'axios';
 
 function Map({ location, zoomLevel }) {
+  const [parks, setParks] = React.useState([]);
+  React.useEffect(() => {
+    // axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search', {
+    //   json: true,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Authorization: 'Bearer FCjYuGUU6sDdV4pbWxqy23I_UsG730pGsK6b5euAEsgmoU6l3UVN2YR5WfIhuiDIZAxfwBxulDU7XUoOGXpbAPb__VPZFuOTo5qY4eNNSsf8LpPqe9GiXFp1rFJrYHYx',
+    //     accept: 'application/json',
+    //     'x-requested-with': 'xmlhttprequest',
+    //     'Access-Control-Allow-Origin': '*',
+    //   },
+    // }).then((res) => {
+    //   setParks(res);
+    // });
+    axios.get('localhost:3000/yelp');
+  });
+
+  function getMapBounds(map, maps, locations) {
+    const bounds = new maps.LatLngBounds();
+    locations.forEach((location) => {
+      bounds.extend(
+        new maps.LatLng(location.latitude, location.longitude),
+      );
+    });
+    return bounds;
+  }
+
+  function bindResizeListener(map, maps, bounds) {
+    maps.event.addDomListenerOnce(map, 'idle', () => {
+      maps.event.addDomListener(window, 'resize', () => {
+        map.fitBounds(bounds);
+      });
+    });
+  }
+
+  function apiIsLoaded(map, maps, locations) {
+    if (map) {
+      const bounds = getMapBounds(map, maps, locations);
+      map.fitBounds(bounds);
+      bindResizeListener(map, maps, bounds);
+    }
+  }
+
   return (
     <div className="map">
-      <h2 className="map-h2">Come Visit Us At Our Campus</h2>
 
       <div className="google-map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCSlzy473wIHHYArtLvphiwprDnzA3myKE' }}
-          defaultCenter={33.8797367240159, -117.96356006545523}
-          defaultZoom={zoomLevel}
-        >
-          <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          />
-        </GoogleMapReact>
+          defaultCenter={{
+            lat: 33.8797367240159,
+            lng: -117.96356006545523,
+          }}
+          defaultZoom={13}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps, parks)}
+        />
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,18 +1,33 @@
-const express = require("express"),
-  cors = require("cors"),
-  morgan = require("morgan"),
-  PORT = 3000,
-  path = require("path"),
-  router = require("./router"),
-  app = express();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-app.use(morgan("dev")).use(cors()).use(express.json());
-app.use("/app", router);
-app.use(express.static(path.join(__dirname, "../client/dist")));
+const PORT = 3000;
+const path = require('path');
+const router = require('./router');
+
+const app = express();
+const axios = require('axios');
+
+app.use(morgan('dev')).use(cors()).use(express.json());
+app.use('/app', router);
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+app.get('/yelp', (req, res) => {
+  axios.get('https://api.yelp.com/v3/businesses/search', {
+    json: true,
+    headers: {
+      authorization: 'Bearer FCjYuGUU6sDdV4pbWxqy23I_UsG730pGsK6b5euAEsgmoU6l3UVN2YR5WfIhuiDIZAxfwBxulDU7XUoOGXpbAPb__VPZFuOTo5qY4eNNSsf8LpPqe9GiXFp1rFJrYHYx',
+    },
+  }).then((result) => {
+    console.log(result);
+    res.send(result);
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
