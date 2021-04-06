ca7e21dfa1bc35fa684a1d525ae0a90caf29ae62
jpdavid <justinepdavid@gmail.com>
2021-04-06 15:33:02 -0700

    Conditionally rendering individual message page on message/photo click; created back button to go back to inbox

--- client/src/components/Messages/Inbox.jsx
+++ client/src/components/Messages/Inbox.jsx
@@ -3,72 +3,74 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const Inbox = ({ matches, matchesPhotos }) => {
-  console.log('matches', matches);
-  console.log('matchesPhotos', matchesPhotos);
-  // getMatchPhotos = () => {
-  // };
+  // console.log('matches', matches);
+  // console.log('matchesPhotos', matchesPhotos);
+  const [messageMode, setMessageMode] = useState(false);
 
-  // useEffect(() => {
-  //   // console.log('matches', matches);
-  //   // const fetchData = async () => {
-  //   //   const data = await getMatchPhotos();
-  //   //   setMatches(data);
-  //   const matchPhotos = [];
-  //   matches.map((match) => axios.get(`/app/users/photos/${match.user_id}`)
-  //     .then((results) => {
-  //       matchPhotos.push(results.data);
-  //     })
-  //     .catch((err) => console.log(err)));
-  //   setMatches(matchPhotos);
-  //   // };
-  //   // fetchData();
-  // }, [setMatches]);
+  const onMessageClick = () => {
+    setMessageMode(!messageMode);
+  };
+
+  const renderMessageMode = () => (
+    <div>
+      <button type="button" onClick={onMessageClick}>Back to Inbox</button>
+      DMs HERE
+    </div>
+  );
 
   return (
     <div id="inbox-container">
       <br />
-      <div id="matches-container">
-        {matchesPhotos.map((match) => (
-          <div className="match-container" key={match[0].user_id}>
-            <span>Human and Dog</span>
+      {!messageMode
+        ? (
+          <div>
+            <div id="matches-container">
+              {matchesPhotos.map((match) => (
+                <div className="match-container" key={match[0].user_id}>
+                  <span>Human and Dog</span>
+                  <br />
+                  <div className="match-photos-container">
+                    <img
+                      className="human-photos"
+                      alt="human"
+                      src={match[0].url}
+                    />
+                    <img
+                      className="dog-photos"
+                      alt="dog"
+                      src={match[1].url}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
             <br />
-            <div className="match-photos-container">
-              <img
-                className="human-photos"
-                alt="human"
-                src={match[0].url}
-              />
-              <img
-                className="dog-photos"
-                alt="dog"
-                src={match[1].url}
-              />
+            <br />
+            <div id="messages-outer-container">
+              <div id="messages-container" onClick={onMessageClick}>
+                Inbox HERE:
+                {matchesPhotos.map((match) => (
+                  <div className="message-container" key={match[0].user_id}>
+                    <div className="messages-photos-container">
+                      <img
+                  className="human-photos-small"
+                  alt="human"
+                  src={match[0].url}
+                />
+                      <img
+                  className="dog-photos-small"
+                  alt="dog"
+                  src={match[1].url}
+                />
+                    </div>
+                    Make the first move! Be bold, write your own story, and get ready to meet the person and dog of your dreams...
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        ))}
-      </div>
-      <br />
-      <br />
-      <div id="messages-container">
-        Inbox HERE:
-        {matchesPhotos.map((match) => (
-          <div className="message-container" key={match[0].user_id}>
-            <div className="messages-photos-container">
-              <img
-                className="human-photos-small"
-                alt="human"
-                src={match[0].url}
-              />
-              <img
-                className="dog-photos-small"
-                alt="dog"
-                src={match[1].url}
-              />
-            </div>
-            Message HERE dkajgfhaiusdhgiuahegoi
-          </div>
-        ))}
-      </div>
+        )
+        : renderMessageMode()}
     </div>
   );
 };
