3ff49ad0cf7cd31a7bcfb022906a944e00975248
jpdavid <justinepdavid@gmail.com>
2021-04-06 20:18:55 -0700

    Implemented onMessageClick function on all clickable components to open DMs; saving index of clicked component into currentMessageId state

--- client/src/components/Messages/Chat.jsx
+++ client/src/components/Messages/Chat.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 const Chat = () => {
-
+  // const matchUserId = matchesPhotos[currentMessageId][0].user_id;
+  // return (
+  //   <div>
+  //     <button type="button" onClick={onMessageClick}>Back to Inbox</button>
+  //     <br />
+  //     <br />
+  //     DMs HERE
+  //     {console.log('matchesphotos', matchUserId)}
+  //     <img alt="human" src={matchesPhotos[currentMessageId][0].url} />
+  //     <img alt="dog" src={matchesPhotos[currentMessageId][1].url} />
+  //   </div>
+  // );
   return (
     <div>
       Hello from Chat
--- client/src/components/Messages/Inbox.jsx
+++ client/src/components/Messages/Inbox.jsx
@@ -1,52 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import Chat from './Chat';
 
 const Inbox = ({
   currentUser, matches, matchesPhotos, allMessages,
 }) => {
   // console.log('matches', matches);
   // console.log('currentUser', currentUser.id);
-  console.log('matchesPhotos', matchesPhotos);
+  // console.log('matchesPhotos', matchesPhotos);
   const [messageMode, setMessageMode] = useState(false);
   const [currentMessageId, setCurrentMessageId] = useState(0);
 
   const onMessageClick = (e) => {
     setMessageMode(!messageMode);
-    console.log(e.target);
+    // console.log(e.target.getAttribute('name'));
     setCurrentMessageId(e.target.getAttribute('name'));
   };
 
-  const renderMessageMode = () => (
-    <div>
-      <button type="button" onClick={onMessageClick}>Back to Inbox</button>
-      <br />
-      <br />
-      DMs HERE
-    </div>
-  );
+  const renderMessageMode = () => {
+    const matchUserId = matchesPhotos[currentMessageId][0].user_id;
+    return (
+      <div>
+        <button type="button" onClick={onMessageClick}>Back to Inbox</button>
+        <br />
+        <br />
+        DMs HERE
+        {console.log('matchesphotos', matchUserId)}
+        <img alt="human" src={matchesPhotos[currentMessageId][0].url} />
+        <img alt="dog" src={matchesPhotos[currentMessageId][1].url} />
+      </div>
+    );
+  };
 
-  (
+  return (
     <div id="inbox-container">
       <br />
       {!messageMode
         ? (
           <div>
             <div id="matches-container">
-              {matchesPhotos.map((match) => (
-                <div className="match-container" key={match[0].user_id} name={(match[0].user_id)} onClick={onMessageClick}>
+              {matchesPhotos.map((match, index) => (
+                <div className="match-container" key={match[0].user_id}>
                   <span>Human and Dog</span>
                   <br />
-                  <div className="match-photos-container" >
+                  <div className="match-photos-container">
                     <img
                       className="human-photos"
                       alt="human"
                       src={match[0].url}
+                      name={index}
+                      onClick={onMessageClick}
                     />
                     <img
                       className="dog-photos"
                       alt="dog"
                       src={match[1].url}
+                      name={index}
+                      onClick={onMessageClick}
                     />
                   </div>
                 </div>
@@ -59,25 +69,27 @@ const Inbox = ({
                 Message Queue (
                 {matches.length}
                 )
-                {matchesPhotos.map((match) => (
+                {matchesPhotos.map((match, index) => (
                   <div className="message-container" key={match[0].user_id} name={match[0].user_id} onClick={onMessageClick}>
-                    <div className="messages-photos-container">
+                    <div className="messages-photos-container" name={index} onClick={onMessageClick}>
                       <img
                         className="human-photos-small"
                         alt="human"
                         src={match[0].url}
+                        name={index}
+                        onClick={onMessageClick}
                       />
                       <img
                         className="dog-photos-small"
                         alt="dog"
                         src={match[1].url}
+                        name={index}
+                        onClick={onMessageClick}
                       />
                     </div>
-                    <div className="name-message-container">
+                    <div className="name-message-container" name={index} onClick={onMessageClick}>
                       {/* {console.log('allmessages at userid', allMessages[match[0].user_id])} */}
-                      <div>
-                        <b>Human name and Dog name</b>
-                      </div>
+                      <div name={index} onClick={onMessageClick} style={{fontWeight: 'bold'}}>Human name and Dog name</div>
                       <div>
                         {(allMessages[match[0].user_id].length !== 0)
                           ? (
@@ -94,9 +106,11 @@ const Inbox = ({
             </div>
           </div>
         )
-        : renderMessageMode()}
+        : <Chat />}
     </div>
   );
+};
+
 Inbox.propTypes = {
   currentUser: PropTypes.objectOf(
     PropTypes.oneOfType([
