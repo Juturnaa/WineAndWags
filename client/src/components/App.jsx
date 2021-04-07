c00998074ad14458237b17853f2e5338b8343b93
MAI <nmai601@gmail.com>
2021-04-07 11:29:09 -0700

    Fixed App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,8 +6,10 @@ import NavBar from './Navbar';
 import breedData from '../dummyData/dogBreed';
 import Map from './Map';
 import Register from './Register';
+import Landing from './Landing';
 
 const App = () => {
+  let [user_id, setUserId] =useState(null);
   const [currentUser, setCurrentUser] = useState({});
   const [currentDogs, setCurrentDogs] = useState([]);
   const [breeds, setBreeds] = useState(breedData);
@@ -17,26 +19,7 @@ const App = () => {
   const [matches, setMatches] = useState([]);
   const [matchesInfo, setMatchesInfo] = useState([]);
   const [matchesPhotos, setMatchesPhotos] = useState([]);
-
-  useEffect(() => {
-    const dogsimages = [];
-    const store = [];
-    if (dogsPhoto.length > 0) {
-      for (let i = 0; i < dogsPhoto.length; i++) {
-        const ind = store.indexOf(dogsPhoto[i].dog_id);
-        if (ind > -1) {
-          dogsimages[ind][dogsPhoto[i].dog_id].push(dogsPhoto[i]);
-        } else {
-          const dogsKey = {};
-          dogsKey[dogsPhoto[i].dog_id] = [dogsPhoto[i]];
-          dogsimages.push(dogsKey);
-          store.push(dogsPhoto[i].dog_id);
-        }
-      }
-    }
-    setDogsImg(dogsimages);
-  }, [dogsPhoto]);
-
+  
   const getRandomUser = (filters) => {
     let random;
     axios.get('/app/users/random-profile', { params: { filters } })
@@ -70,63 +53,88 @@ const App = () => {
         console.log(err);
       });
   };
-  useEffect(() => {
-    axios.all([
-      axios.get('/app/users/my-profile/sophiaacheong5@gmail.com'),
-      axios.get('/app/users/photos/7'),
-    ])
-      .then(axios.spread((one, two) => {
-        setCurrentUser(one.data);
-        setCurrentDogs(one.data.dogs_info);
-        const human = [];
-        const dogs = [];
-        for (let i = 0; i < two.data.length; i++) {
-          if (two.data[i].dog_id === null) {
-            human.push(two.data[i]);
-          } else {
-            dogs.push(two.data[i]);
+
+  if(user_id) {
+      useEffect(() => {
+        const dogsimages = [];
+        const store = [];
+        if (dogsPhoto.length > 0) {
+          for (let i = 0; i < dogsPhoto.length; i++) {
+            const ind = store.indexOf(dogsPhoto[i].dog_id);
+            if (ind > -1) {
+              dogsimages[ind][dogsPhoto[i].dog_id].push(dogsPhoto[i]);
+            } else {
+              const dogsKey = {};
+              dogsKey[dogsPhoto[i].dog_id] = [dogsPhoto[i]];
+              dogsimages.push(dogsKey);
+              store.push(dogsPhoto[i].dog_id);
+            }
           }
         }
-        setHumanPhoto(human);
-        setDogsPhoto(dogs);
-      }))
-      .catch((err) => console.error(err));
-  }, []);
-
-  useEffect(() => {
-    axios.get(`/app/${currentUser.id}/matches`)
-      .then((results) => {
-        setMatches(results.data);
-      })
-      .catch((err) => console.log(err));
-  }, [currentUser]);
-
-  useEffect(() => {
-    const matchPhotos = [];
-    matches.map((match) => axios.get(`/app/users/photos/${match.user_id}`)
-      .then((results) => {
-        matchPhotos.push(results.data);
-      })
-      .catch((err) => console.log(err)));
-    setMatchesPhotos(matchPhotos);
-  }, [matches]);
+        setDogsImg(dogsimages);
+      }, [dogsPhoto]);
+    
+      useEffect(() => {
+        axios.all([
+          axios.get('/app/users/my-profile/sophiaacheong5@gmail.com'),
+          axios.get('/app/users/photos/7'),
+        ])
+          .then(axios.spread((one, two) => {
+            setCurrentUser(one.data);
+            setCurrentDogs(one.data.dogs_info);
+            const human = [];
+            const dogs = [];
+            for (let i = 0; i < two.data.length; i++) {
+              if (two.data[i].dog_id === null) {
+                human.push(two.data[i]);
+              } else {
+                dogs.push(two.data[i]);
+              }
+            }
+            setHumanPhoto(human);
+            setDogsPhoto(dogs);
+          }))
+          .catch((err) => console.error(err));
+      }, []);
+    
+      useEffect(() => {
+        axios.get(`/app/${currentUser.id}/matches`)
+          .then((results) => {
+            setMatches(results.data);
+          })
+          .catch((err) => console.log(err));
+      }, [currentUser]);
+    
+      useEffect(() => {
+        const matchPhotos = [];
+        matches.map((match) => axios.get(`/app/users/photos/${match.user_id}`)
+          .then((results) => {
+            matchPhotos.push(results.data);
+          })
+          .catch((err) => console.log(err)));
+        setMatchesPhotos(matchPhotos);
+      }, [matches]);
+  }
 
   return (
-    // <div>
-    //   <NavBar
-    //     likePhoto={likePhoto}
-    //     likeProfile={likeProfile}
-    //     humanPhoto={humanPhoto}
-    //     dogsImg={dogsImg}
-    //     getRandomUser={getRandomUser}
-    //     currentUser={currentUser}
-    //     breeds={breeds}
-    //     currentDogs={currentDogs}
-    //     matches={matches}
-    //     matchesPhotos={matchesPhotos}
-    //   />
-    // </div>
-    <Register />
+    <div>    
+    {user_id ? 
+        <NavBar
+          likePhoto={likePhoto}
+          likeProfile={likeProfile}
+          humanPhoto={humanPhoto}
+          dogsImg={dogsImg}
+          getRandomUser={getRandomUser}
+          currentUser={currentUser}
+          breeds={breeds}
+          currentDogs={currentDogs}
+          matches={matches}
+          matchesPhotos={matchesPhotos}
+        />
+    :
+      <Landing setUserId={setUserId}/>        
+    }
+    </div>
   );
 };
 
