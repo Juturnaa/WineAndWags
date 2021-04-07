eec0f61c08d60f61ba0fa8a76fd91d885dc208ca
jpdavid <justinepdavid@gmail.com>
2021-04-06 20:56:35 -0700

    Rendering human and dog photos in Chat component based on user clicked

--- client/src/components/Messages/Chat.jsx
+++ client/src/components/Messages/Chat.jsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Chat = ({ matchesPhotos, messageMode, currentMessageId, allMessages, onMessageClick, }) => {
+const Chat = ({
+  matchesPhotos, messageMode, currentMessageId, allMessages, onMessageClick,
+}) => {
   const matchUserId = matchesPhotos[currentMessageId][0].user_id;
   return (
     <div>
       <button type="button" onClick={onMessageClick}>Back to Inbox</button>
       <br />
       <br />
-      DMs HERE
-      {console.log('matchesphotos', matchUserId)}
-      <img alt="human" src={matchesPhotos[currentMessageId][0].url} />
-      <img alt="dog" src={matchesPhotos[currentMessageId][1].url} />
+      <div id="chat-container">
+        <div id="chat-images-container">
+          <img className="chat-human-photo" alt="human" src={matchesPhotos[currentMessageId][0].url} />
+          <img className="chat-dog-photo" alt="dog" src={matchesPhotos[currentMessageId][1].url} />
+          <div id="chat-names">Human and Dog</div>
+        </div>
+        <br />
+        <br />
+        <div id="direct-messages-container">
+          CHAT HERE
+        </div>
+      </div>
     </div>
   );
-  // return (
-  //   <div>
-  //     Hello from Chat
-  //   </div>
-  // )
 };
 
 Chat.propTypes = {
