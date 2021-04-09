3fe9c473ac9ebde97b6cc2ee59a0d92c176d7af6
hyungjooyoon <hyungjooyoon@gmail.com>
2021-04-09 13:36:26 -0700

    implemented socket and video player.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,6 +8,8 @@ import breedData from '../dummyData/dogBreed';
 import Landing from './Landing';
 import Register from './Register';
 import ReviewModal from './Homepage/ReviewModal';
+import { ContextProvider } from './Video/SocketContext';
+import Video from './Video/Video';
 
 const App = () => {
   const [currentUserID, setCurrentID] = useState(7);
@@ -230,7 +232,7 @@ const App = () => {
   return (
     <div>
       {reviewModal ? <ReviewModal reviewModal={reviewModal} setReviewModal={setReviewModal} appointment={appointment || ''} /> : null}
-      <NavBar
+      {/* <NavBar
         likePhoto={likePhoto}
         likeProfile={likeProfile}
         humanPhoto={humanPhoto || ''}
@@ -249,7 +251,10 @@ const App = () => {
         potientialDogsImg={potientialDogsImg}
         showNotifs={showNotifs}
         setShowNotifs={setShowNotifs}
-      />
+      /> */}
+      <ContextProvider>
+        <Video />
+      </ContextProvider>
     </div>
   );
 };
