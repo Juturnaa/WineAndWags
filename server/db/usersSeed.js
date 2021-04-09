1fc08cd21937ad4b4d0e464484fd0629ffe483bc
Michael Banuelos <mbanuelos1995@gmail.com>
2021-04-09 16:51:45 -0700

    updated seed functions

--- server/db/usersSeed.js
+++ server/db/usersSeed.js
@@ -2,9 +2,143 @@
 /* eslint-disable camelcase */
 const bcrypt = require("bcryptjs");
 const db = require("./index.js");
+const breed = require("../../client/src/dummyData/dogBreed");
 
-const maleNames = [];
-const femaleNames = [];
+const malePhotos = [
+  "https://image.shutterstock.com/image-photo/portrait-smiling-red-haired-millennial-600w-1194497251.jpg",
+  "https://image.shutterstock.com/image-photo/head-shot-portrait-smiling-middle-600w-1339318991.jpg",
+  "https://image.shutterstock.com/image-photo/side-view-young-happy-smiling-600w-462825271.jpg",
+  "https://image.shutterstock.com/image-photo/halfturned-side-profile-close-view-600w-790169857.jpg",
+  "https://image.shutterstock.com/image-photo/portrait-smiling-african-american-student-600w-1194497215.jpg",
+  "https://image.shutterstock.com/image-photo/african-man-portrait-silhouette-isolated-600w-432271696.jpg",
+  "https://image.shutterstock.com/image-photo/portrait-young-smiling-handsome-bearded-600w-1906254982.jpg",
+  "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-600w-1640944705.jpg",
+  "https://image.shutterstock.com/image-photo/close-side-profile-photo-intelligent-600w-1447435748.jpg",
+];
+const femalePhotos = [
+  "https://image.shutterstock.com/image-photo/close-side-portrait-healthy-young-600w-674516284.jpg",
+  "https://image.shutterstock.com/image-photo/young-beautiful-brunette-woman-wearing-600w-1727105977.jpg",
+  "https://image.shutterstock.com/image-photo/young-african-american-girl-wearing-600w-1893704815.jpg",
+  "https://image.shutterstock.com/image-photo/woman-shirt-studio-profile-isolated-600w-520346509.jpg",
+  "https://image.shutterstock.com/image-photo/close-profile-half-face-portrait-600w-1018670407.jpg",
+  "https://image.shutterstock.com/image-photo/side-portrait-smiling-asian-woman-600w-1399301462.jpg",
+  "https://image.shutterstock.com/image-photo/beautiful-redhead-woman-freckles-studio-600w-789036601.jpg",
+  "https://image.shutterstock.com/image-photo/profile-side-view-portrait-nice-600w-1619251795.jpg",
+];
+const maleBios = [
+  "Greetings!",
+  "hello world",
+  "Used to work at Jamba Juice!",
+  "im all vibed up!",
+  "YEEEEEHAWEWWWWW",
+];
+const femaleBios = [
+  "I wish I was younger!",
+  "AQUARIUS",
+  "Love Going Outside",
+  "looking for a wonderful conversation...",
+  "TOTAL WINE FIEND",
+];
+
+const dogPhotos = [
+  "https://images.dog.ceo//breeds//terrier-russell//little1.jpg",
+  "https://images.dog.ceo//breeds//cattledog-australian//IMG_3668.jpg",
+  "https://images.dog.ceo//breeds//cotondetulear//IMAG1063.jpg",
+  "https://images.dog.ceo//breeds//kuvasz//n02104029_2504.jpg",
+  "https://images.dog.ceo//breeds//retriever-chesapeake//n02099849_953.jpg",
+  "https://images.dog.ceo//breeds//boxer//n02108089_2791.jpg",
+  "https://images.dog.ceo//breeds//finnish-lapphund//mochilamvan.jpg",
+  "https://images.dog.ceo//breeds//beagle//DSC05086.JPG",
+  "https://images.dog.ceo//breeds//dhole//n02115913_1010.jpg",
+  "https://images.dog.ceo//breeds//poodle-toy//n02113624_265.jpg",
+  "https://images.dog.ceo//breeds//pug//DSCF7495-2.jpg",
+];
+
+const dogNames = [
+  "Charlie",
+  "Goofie",
+  "Takito",
+  "Mochi",
+  "Rocky",
+  "Wolfie",
+  "Alpha",
+  "Kun",
+  "Michael",
+  "Jake",
+  "Hyungjoo",
+  "Cloud",
+  "Chloe",
+  "Ruby",
+  "Shake",
+  "Oliver",
+  "Pierre",
+  "Revan",
+  "Charlie",
+  "Goofie",
+  "Taquito",
+  "Takito",
+  "Mochi",
+  "Rocky",
+  "Wolfie",
+  "Alpha",
+  "Kun",
+  "Michael",
+  "Jake",
+  "Hyungjoo",
+  "Cloud",
+  "Chloe",
+  "Ruby",
+  "Shake",
+  "Oliver",
+  "Pierre",
+  "Revan",
+];
+
+const dogGenders = ["M", "F"];
+
+const dogBios = [
+  "I love to play fetch",
+  "I run around the house and I like to play with my hooman",
+  "The park is my favorite place to go",
+  "I nap 24/7",
+  "Beef is my favorite meat, along with a side of veggies",
+  "Hiking is my favorite thing to do with my hoodman every Sunday morning",
+];
+
+const hypo = [true, false];
+
+const neutered = [true, false];
+
+const rating = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const dogAges = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+];
+
+const dogSizes = ["XS", "S", "M", "L", "XL"];
+
+const healthy = [true, false];
+
+const maleNames = ["Derek", "Nader", "Ronald", "Kun", "Marcus", "Timothy"];
+const femaleNames = ["Sarah", "Lisa", "Veronica", "Latrella"];
 const ages = [
   18,
   19,
@@ -146,29 +280,45 @@ const zipcodes = [
 ];
 
 const genders = ["M", "F", "All"];
-const maleBios = [];
-const femaleBios = [];
-const bios = ["hey my name is triko.", "YEEEEEHAWEWWWWW", "im all vibed up!"];
+const photoGender = ["M", "F"];
 const password = "password";
+const cities = [
+  "Pasadena",
+  "Silver Lake",
+  "Glendale",
+  "Santa Monica",
+  "Arcadia",
+  "Temple City",
+];
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const seedUsers = () => {
   for (let i = 1; i <= 1000; i++) {
-    const name = names[getRandom(0, names.length)];
-    const gender = genders[getRandom(0, genders.length)];
-    const bio = bios[getRandom(0, bios.length)];
+    let name = maleNames[getRandom(0, maleNames.length)];
+    let bio = femaleBios[getRandom(0, maleBios.length)];
+    let photo;
+
+    const gender = photoGender[getRandom(0, genders.length)];
+    if (gender === "M" || "All") {
+      name = maleNames[getRandom(0, maleNames.length)];
+      bio = maleBios[getRandom(0, maleBios.length)];
+    } else {
+      name = femaleNames[getRandom(0, femaleNames.length)];
+      bio = femaleBios[getRandom(0, maleBios.length)];
+    }
     const email = `sophiaacheong${i}@gmail.com`;
     const age = ages[getRandom(0, ages.length)];
     const zipcode = zipcodes[getRandom(0, zipcodes.length)];
     const searched_as = genders[getRandom(0, genders.length)];
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
-    const city = "LA";
+    const city = cities[getRandom(0, cities.length)];
     console.log(i);
     db.query(
-      'INSERT INTO waw.users("name", bio, email, "password", age, zipcode, city, searched_as) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+      'INSERT INTO waw.users(id, "name", bio, email, "password", age, zipcode, city, searched_as) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)',
       [
+        `${i}`,
         `${name}`,
         `${bio}`,
         `${email}`,
@@ -180,7 +330,68 @@ const seedUsers = () => {
       ],
       (err, res) => console.log("done!")
     );
+
+    for (let k = 1; k < 2; k++) {
+      if (gender === "M" || "All") {
+        photo = malePhotos[getRandom(0, malePhotos.length)];
+      } else {
+        photo = femalePhotos[getRandom(0, femalePhotos.length)];
+      }
+      db.query(
+        `INSERT INTO waw.photos(user_id, dog_id, url) VALUES (${i}, ${null}, '${photo}')`
+      );
+    }
   }
 };
 
+const seedDogs = () => {
+  let dogId = 1;
+
+  for (let i = 1; i <= 1000; i++) {
+    let dogAmount = getRandom(0, 3);
+
+    for (let j = 0; j < dogAmount; j++) {
+      const dogName = dogNames[getRandom(0, dogNames.length)];
+      const dogGender = dogGenders[getRandom(0, dogGenders.length)];
+      const dogBio = dogBios[getRandom(0, dogBios.length)];
+      const hypoOptions = Math.random() < 0.5;
+      const neuteredOptions = Math.random() < 0.5;
+      const ratingOptions = rating[getRandom(0, rating.length)];
+      const owner_id = i + 1;
+      const dogAge = dogAges[getRandom(0, dogAges.length)] + 1;
+      const size = dogSizes[getRandom(0, dogSizes.length)];
+      const breeds = breed[getRandom(0, breed.length)];
+      const healthy = Math.random() < 0.5;
+      db.query(
+        "INSERT INTO waw.dogs(id, name, gender, bio, hypo, neutered, rating, owner_id, age, size, breed, healthy) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+        [
+          `${dogId}`,
+          `${dogName}`,
+          `${dogGender}`,
+          `${dogBio}`,
+          `${hypoOptions}`,
+          `${neuteredOptions}`,
+          `${ratingOptions}`,
+          `${owner_id}`,
+          `${dogAge}`,
+          `${size}`,
+          `${breeds}`,
+          `${healthy}`,
+        ],
+        (err, res) => console.log("done!")
+      );
+
+      for (let k = 1; k <= dogAmount; k++) {
+        let photo = dogPhotos[getRandom(0, dogPhotos.length)];
+        db.query(
+          `INSERT INTO waw.photos(user_id, dog_id, url) VALUES (${i}, ${dogId}, '${photo}')`
+        );
+      }
+      dogId++;
+    }
+    dogId++;
+  }
+};
 seedUsers();
+
+seedDogs();
