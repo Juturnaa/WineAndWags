3988ac5ac854273d8382ae6fcfa40795e5065696
MAI <nmai601@gmail.com>
2021-04-09 13:31:04 -0700

    Changed styling on registration pages and minor improvements on the rest of the site

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -77,11 +77,11 @@ function NavBar({
           </div>
         </div>
         <div className="nav-icons">
-          <NavLink className="nav-icon" exact to="/home" onClick={() => { setEdit(false); setShowNotifs(false); }}><i className="fas fa-home" /></NavLink>
-          <button style={{ background: 'none', border: 'none' }} className="nav-icon" onClick={() => { setShowNotifs(!showNotifs); setEdit(false); }}><i className="far fa-bell" /></button>
-          <NavLink className="nav-icon" exact to="/inbox" onClick={() => { setEdit(false); setShowNotifs(false); }}><i className="far fa-envelope" /></NavLink>
-          <NavLink className="nav-icon" exact to="/map" onClick={() => { setEdit(false); setShowNotifs(false); }}><i className="far fa-map" /></NavLink>
-          <a className="nav-icon" onClick={() => { setEdit(!edit); setShowNotifs(false); }}>
+          <NavLink className="nav-icon" exact to="/home"><i className="fas fa-home" /></NavLink>
+          <button style={{ background: 'none', border: 'none' }} className="nav-icon" onBlur={()=> setShowNotifs(false)} onClick={() => setShowNotifs(!showNotifs)}><i className="far fa-bell" /></button>
+          <NavLink className="nav-icon" exact to="/inbox"><i className="far fa-envelope" /></NavLink>
+          <NavLink className="nav-icon" exact to="/map"><i className="far fa-map" /></NavLink>
+          <a className="nav-icon" onClick={() => setEdit(!edit)} onBlur={()=> setEdit(false)}>
             {humanPhoto.length ? (
               <div
                 className="profile-thumbnail"
