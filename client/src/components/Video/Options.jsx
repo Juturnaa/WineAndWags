7beec6365f023b31edad20749b96f96f1d6a0362
hyungjooyoon <hyungjooyoon@gmail.com>
2021-04-10 15:47:00 -0700

    Added Answer button. and Set name for caller.

--- client/src/components/Video/Options.jsx
+++ client/src/components/Video/Options.jsx
@@ -1,47 +1,56 @@
 import React from 'react';
 import { SocketContext } from './SocketContext';
 
-function Options({ children }) {
+function Options({ username }) {
   const {
-    me, callAccepted, name, setName, callEnded, leaveCall, callUser,
+    me, callAccepted, name, setName, callEnded, leaveCall, callUser, answerCall, call
   } = React.useContext(SocketContext);
 
   const [idToCall, setIdToCall] = React.useState('');
-  console.log(me);
+  setName(username);
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}} >
+    <div style={{
+      display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+    }}
+    >
       <div>
         {`id: ${me}`}
       </div>
       <div>
         Make a Call
       </div>
-      <div style={{display: 'flex', flexDirection: 'row'}} >
-      <input
-        type="text"
-        value={idToCall}
-        onChange={(e) => setIdToCall(e.target.value)}
-      />
-      {callAccepted && !callEnded ? (
-       
-        
-        <button
-          type="button"
-          onClick={leaveCall}
-        >
-          <i className="fas fa-phone-slash"></i>
-        </button>
-      ) : (
-        <button
-          type="button"
-          onClick={() => callUser(idToCall)}
-        >
-          <i className="fas fa-phone"></i>
-        </button>
-      )}
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <input
+          type="text"
+          value={idToCall}
+          onChange={(e) => setIdToCall(e.target.value)}
+        />
+        {callAccepted && !callEnded ? (
+
+          <button
+            type="button"
+            onClick={leaveCall}
+          >
+            <i className="fas fa-phone-slash" />
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => callUser(idToCall)}
+          >
+            <i className="fas fa-phone" />
+          </button>
+        )}
+        {call.isReceivedCall && !callAccepted && (
+          <button
+            type="button"
+            onClick={() => answerCall}
+          >
+            Answer Call
+          </button>
+        )}
       </div>
-
-
     </div>
   );
 }
